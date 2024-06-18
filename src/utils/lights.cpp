f127646b688b8512561cd6e6de22cdc0631f3294
albertdyu <albert.dayuan.yu@gmail.com>
2024-06-18 13:28:13 -0400

    Updated ledcAttach for 3.x

--- src/utils/lights.cpp
+++ src/utils/lights.cpp
@@ -5,14 +5,14 @@
  */
 void initLights(){
 // Setup pins
-  ledcSetup(PWM_RED, PWM_FREQ, PWM_RESOLUTION);
-  ledcSetup(PWM_GREEN, PWM_FREQ, PWM_RESOLUTION);
-  ledcSetup(PWM_WHITE, PWM_FREQ, PWM_RESOLUTION);
+  //ledcSetup(PWM_RED, PWM_FREQ, PWM_RESOLUTION);
+  //ledcSetup(PWM_GREEN, PWM_FREQ, PWM_RESOLUTION);
+  //ledcSetup(PWM_WHITE, PWM_FREQ, PWM_RESOLUTION);
   pinMode(IR_PIN, OUTPUT);
 
-  ledcAttachPin(RED_PIN, PWM_RED);
-  ledcAttachPin(GREEN_PIN, PWM_GREEN);
-  ledcAttachPin(WHITE_PIN, PWM_WHITE);
+  ledcAttach(RED_PIN, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttach(GREEN_PIN, PWM_FREQ, PWM_RESOLUTION);
+  ledcAttach(WHITE_PIN, PWM_FREQ, PWM_RESOLUTION);
 
   sleep(1);
   ledcWrite(PWM_RED, 0);
